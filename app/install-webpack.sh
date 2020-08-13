ace5a04ed32a2961381cd3d0671a03ada30620ac
bgoldsbe <briangoldsberry25@gmail.com>
2020-08-13 03:04:48 -0500

    webpack

---
+++ app/install-webpack.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+npm install --save \
+  @babel/core \
+  @babel/plugin-proposal-class-properties \
+  @babel/plugin-proposal-export-default-from \
+  @babel/plugin-proposal-export-namespace-from \
+  @babel/plugin-proposal-optional-chaining \
+  @babel/plugin-transform-runtime \
+  @babel/preset-env \
+  @babel/preset-react \
+  autoprefixer \
+  babel-loader \
+  babel-plugin-add-module-exports \
+  babel-plugin-module-resolver \
+  copy-webpack-plugin \
+  css-loader \
+  extract-text-webpack-plugin \
+  html-webpack-plugin \
+  mini-css-extract-plugin \
+  node-sass \
+  nodemon \
+  resolve-url-loader \
+  sass-loader \
+  style-loader \
+  url-loader \
+  webpack \
+  webpack-cli \
+  webpack-manifest-plugin \
